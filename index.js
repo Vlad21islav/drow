e6694a7f967a94d6898a771489806d6dc34abf9b
Vlad21islav <vmitk@mail.ru>
2023-08-22 15:06:36 +0300

    name

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const str = document.getElementById('str');
 const inp = document.getElementById('inp');
-const language = document.getElementById('language');
+const languages = document.getElementById('language');
 const errors = document.getElementById('errors');
 
 if (str === null || inp === null || language === null || errors === null) {
     console.log('нет одного из элементов');
     } else {
-        str.innerHTML = language.options[language.selectedIndex].text
-        language.addEventListener('input', function() {
-            str.innerHTML = language.options[language.selectedIndex].text
+        str.innerHTML = languages.options[languages.selectedIndex].text
+        languages.addEventListener('input', function() {
+            str.innerHTML = languages.options[languages.selectedIndex].text
         });
     };
 

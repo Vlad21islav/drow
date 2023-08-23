df0e7f0fe2d375efa327d292952be148380c4c01
Vlad21islav <vmitk@mail.ru>
2023-08-23 17:58:37 +0300

    bugs

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const btn = document.getElementById('btn');
 const language_word = document.getElementById('language_word');
 const language_names = document.getElementById('languages');
 const won = document.getElementById('won');
+const btn_inp = document.getElementById('btn_inp');
 
 function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
-  }
+};
 
 const words = [
-    ['приветствие', 'забавный', 'красотка', 'победитель', 'интеллект', 'удивительный', 'надежный', 'эксперимент', 'мелодичный', 'восхитительный', 'растительность', 'демонстрация', 'громадный', 'оригинальный', 'совершенный', 'безопасность', 'инновационный', 'участник', 'эмоциональный', 'многообразие', 'исследование', 'высококачественный', 'образовательный', 'технологический', 'перспективный', 'увлекательный', 'симпатичный', 'настоящий', 'замечательный', 'оригинальность', 'разнообразие', 'креативный', 'эффективность', 'прогрессивный', 'необычный', 'стабильность', 'интересный', 'научный', 'современный', 'фантастический', 'уникальный', 'культурный', 'творческий', 'индивидуальность', 'продуктивность', 'экологический', 'безграничный', 'уверенность', 'просторный', 'гармоничный'],
+    ['приветствие', 'забавный', 'красотка', 'победитель', 'интеллект', 'удивительный', 'надёжный', 'эксперимент', 'мелодичный', 'восхитительный', 'растительность', 'демонстрация', 'громадный', 'оригинальный', 'совершённый', 'безопасность', 'инновационный', 'участник', 'эмоциональный', 'многообразие', 'исследование', 'высококачественный', 'образовательный', 'технологический', 'перспективный', 'увлекательный', 'симпатичный', 'настоящий', 'замечательный', 'оригинальность', 'разнообразие', 'креативный', 'эффективность', 'прогрессивный', 'необычный', 'стабильность', 'интересный', 'научный', 'современный', 'фантастический', 'уникальный', 'культурный', 'творческий', 'индивидуальность', 'продуктивность', 'экологический', 'безграничный', 'уверенность', 'просторный', 'гармоничный'],
     ['greeting', 'funny', 'beauty', 'winner', 'intelligence', 'amazing', 'reliable', 'experiment', 'melodic', 'delightful', 'vegetation', 'demonstration', 'enormous', 'original', 'perfect', 'safety', 'innovative', 'participant', 'emotional', 'diversity', 'research', 'high-quality', 'educational', 'technological', 'promising', 'captivating', 'cute', 'genuine', 'wonderful', 'originality', 'variety', 'creative', 'efficiency', 'progressive', 'unusual', 'stability', 'interesting', 'scientific', 'modern', 'fantastic', 'unique', 'cultural', 'creative', 'individuality', 'productivity', 'ecological', 'boundless', 'confidence', 'spacious', 'harmonious']
 ];
 
@@ -21,26 +22,24 @@ let shuffled_words = shuffle(words[1])
 
 const messages = [
     {
+        ANSWER: `ответить`,
         LANGUGE: `язык`,
         PLAY: `играть`,
         FIRST_YOU_HAVE_LOST: `Вы проиграли, ваш рекорд $1`,
         YOU_HAVE_LOST: `Вы проиграли, ваш рекорд $1, среднее время записи слова - $2 секунд, общее время - $3 секунд`,
         YOU_WON: `Слова закончились, вы выиграли, ваш рекорд $1, среднее время записи слова - $2 секунд, общее время - $3 секунд`,
         REMEMBER_WORD: `Запомните слово:`,
-        ENTER_WORD: `Введите слово правильно:`,
-        FIRST_TIME_IS_UP: `время вышло, ваш рекорд $1`,
-        TIME_IS_UP: `время вышло, ваш рекорд $1, среднее время записи слова - $2 секунд, общее время - $3 секунд`,
+        ENTER_WORD: `Введите слово правильно:`
     },
     {
+        ANSWER: `answer`,
         LANGUGE: `language`,
         PLAY: `play`,
         FIRST_YOU_HAVE_LOST: `You lost, your record is $1`,
         YOU_HAVE_LOST: `You lost, your record is $1, the average time of writing a word is $2 second, total time is $3 seconds`,
         YOU_WON: `The words are over, you have won, your record is $1, the average time of writing a word is $2 seconds, total time is $3 seconds`,
         REMEMBER_WORD: `Remember the word:`,
-        ENTER_WORD: `Enter the word correctly:`,
-        FIRST_TIME_IS_UP: `time is up, your record is $1`,
-        TIME_IS_UP: `time is up, your record is $1, the average time of writing a word is $2 seconds, total time is $3 seconds`,
+        ENTER_WORD: `Enter the word correctly:`
     },
 ];
 
@@ -79,48 +78,79 @@ class Game {
     constructor(logger, shuffled_words) {
         this.logger = logger;
         this.shuffled_words = shuffled_words;
+        this.count = 0;
+        this.overTime = 0;
+        this.startTime = 0;
     }
 
     start() {
         inp.style.display='none';
+        btn_inp.style.display='none';
         languages.addEventListener('input', () => {
             this.logger = new Logger(messages[languages.selectedIndex]); 
             language_word.innerHTML = messages[languages.selectedIndex].LANGUGE;
             btn.value = messages[languages.selectedIndex].PLAY;
-            this.shuffled_words = shuffle(words[languages.selectedIndex])
+            this.shuffled_words = shuffle(words[languages.selectedIndex]);
         });
         btn.addEventListener('click', () => {
+            btn.style.display='none';
+            language_names.style.display='none';
             this.remember_word();
         });
     };
 
     remember_word() {
+        inp.value = ''
         print(this.logger.info('REMEMBER_WORD'), str1);
-        print(this.shuffled_words[0], str2);
-        btn.style.display='none';
-        language_names.style.display='none';
+        print(this.shuffled_words[this.count].split('').reverse().join(''), str2);
         setTimeout(() => {
             this.enter_word();
-        }, 2000);
+        }, 5000);
     };
 
     enter_word() {
+        btn_inp.style.display='';
+        btn_inp.value = messages[languages.selectedIndex].ANSWER;
         print('', str1);
         print('', str2);
         inp.style.display='';
         inp.placeholder = this.logger.info('ENTER_WORD');
+        btn_inp.addEventListener('click', () => {
+            this.enter_word();
+        });
+        inp.addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+                this.enter_word();
+            };
+        });
         setTimeout(() => {
-            this.end()
+            this.end();
         }, 7000);
     };
 
     end() {
-        if (inp.value === this.shuffled_words[0]) {
-            print(this.logger.info('YOU_WON'), won) 
+        btn_inp.style.display='none';
+        if (this.count > shuffled_words.length) {
+            print(this.logger.info('YOU_WON'), won);
+        } else if (inp.value === this.shuffled_words[this.count]) {
+            this.count++;
+            this.remember_word();
         } else {
-            print(this.logger.info('YOU_HAVE_LOST'), errors) 
+            if (this.count === 0) {
+                print(this.logger.info('FIRST_YOU_HAVE_LOST', [this.count]), errors);
+            } else {
+                print(this.logger.info('YOU_HAVE_LOST', [this.count, this.getAverageTime(), this.getTotalTime()]), errors);
+            };
         };
         inp.style.display='none';
+    };
+
+    getAverageTime() {
+        return Math.floor(this.overTime / this.count / 1000 * 100) / 100;
+    };
+
+    getTotalTime() {
+        return Math.floor(this.overTime / 1000 * 100) / 100;
     };
 };
 

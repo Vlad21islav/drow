1160647692ae363d0a6ea3334c4550a8257053b9
Vlad21islav <vmitk@mail.ru>
2023-08-26 19:51:39 +0300

    bugs

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ class Game{
         this.exit_btn = document.getElementById('exit_btn');
         this.result = document.getElementById('result');
         this.words = document.getElementById('words');
-    }
 
-    bugs() {
-        if (this.start_btn === null || this.input === null || this.answer_btn === null || this.exit_btn === null || this.result === null || this.words === null) {
-            console.log('нету одной из переменных')
-        } else {
-
-        }
-    }
-}
+        if (this.start_btn === null) throw new Error('Не найден элемент с id "start_btn"')
+        if (this.input === null) throw new Error('Не найден элемент с id "input"')
+        if (this.answer_btn === null) throw new Error('Не найден элемент с id "answer_btn"')
+        if (this.exit_btn === null) throw new Error('Не найден элемент с id "exit_btn"')
+        if (this.result === null) throw new Error('Не найден элемент с id "result"')
+        if (this.words === null) throw new Error('Не найден элемент с id "words"')
+    };
+};
